e39f9fa051b3bcc8b9e67d36815b58685a72fb23
NTBarz <nathanbbarz@gmail.com>
2025-07-13 16:18:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    var mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Bem-vindo ao meu site! Aqui você pode conhecer mais sobre meus hobbies e interesses.";
+}
